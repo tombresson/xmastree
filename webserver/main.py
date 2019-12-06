4051e3c607f06c5d572a7b23e22a085ee61e775c
Tom Bresson <turfyman@gmail.com>
2019-12-05 23:33:10 -0500

    Add a little bit of API functionailty

--- webserver/main.py
+++ webserver/main.py
@@ -9,15 +9,30 @@ device_id = particle_keys.device_id
 def main():
     song_list = getSongs()
     print(song_list)
+    playSong(4)
 
 
 # Make a request to get the JSON Payload containing the Songs from the Particle API
 def getSongs():
-    r = requests.get('https://api.particle.io/v1/devices/' + device_id + '/songList', params=token)
+    r = requests.get('https://api.particle.io/v1/devices/' + device_id + '/songList',
+                     params=token)
     response_data = json.loads(r.text)
     key = 'result'
     if key in response_data:
         return ([str(i) for i in response_data[key].split(',')])
 
+
+def playSong(idx: int):
+    arg = {'arg': str(idx) }
+    r = requests.post('https://api.particle.io/v1/devices/' + device_id + '/playSong',
+                     params=token, data=arg)
+
+def nextAnimation():
+    r = requests.post('https://api.particle.io/v1/devices/' + device_id + '/nextAnim',
+                     params=token)
+
+def prevAnimation():
+    r = requests.post('https://api.particle.io/v1/devices/' + device_id + '/nextPrev',
+                     params=token)
 
 main()
