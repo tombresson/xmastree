3982c3177642450e2c9970ae860286547b23faa4
Tom Bresson <tom.bresson@gmail.com>
2019-12-08 23:27:16 -0500

    Add credentials lib

---
+++ src/firmware/credentials.cpp
@@ -0,0 +1,80 @@
+/*****************************************************************************
+ * @file    credentials.cpp
+ * @brief   Source code .cpp file for credentials module
+ * @date    2019-DEC-08
+ *
+ * @note <insert notes>
+ *
+ * @ingroup doxy_group
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019, Thomas Bresson
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+*****************************************************************************/
+
+/**
+ * @addtogroup doxy_group
+ * @{
+ */
+
+/* ***************************    Includes     **************************** */
+
+#include "application.h"
+
+#include "credentials.h"
+
+/* ***************************   Definitions   **************************** */
+
+#define ARRAY_SIZE(x) ((sizeof x) / (sizeof *x))
+
+/* ****************************   Structures   **************************** */
+
+/* ***********************   Function Prototypes   ************************ */
+
+/* ***********************   File Scope Variables   *********************** */
+
+
+// Core: remembers the 7 most recently set credentials
+// Photon: remembers the 5 most recently set credentials.
+// Argon: remembers the 10 most recently set credentials.
+const credentialData_t cred_list[] =
+{
+    {.ssid = "", .pw = "", .sec_type = UNSEC},
+    {.ssid = "", .pw = "", .sec_type = UNSEC},
+    {.ssid = "", .pw = "", .sec_type = UNSEC},
+    {.ssid = "", .pw = "", .sec_type = UNSEC},
+    {.ssid = "", .pw = "", .sec_type = UNSEC}
+};
+
+/* ****************************   BEGIN CODE   **************************** */
+
+/* *************************   Public  Functions   ************************ */
+
+// Returns a list of credentials
+credList_t credentialsGetList(void)
+{
+    return (credList_t){ .cred_list = cred_list, .size = ARRAY_SIZE(cred_list)};
+}
+
+/* *************************   Private Functions   ************************ */
+
+/* ************************   Interrupt Functions   *********************** */
